6c2764745e559aca0a8092b57068b0046ec5695d
SergeyBalakirev <balakirev.sergey87@gmail.com>
2020-05-22 14:03:03 +0300

    added product slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function () {
 
+  $(".featured-product__descr-star").rateYo({
+    rating: 4.5,
+    starWidth: "15px",
+    ratedFill: "#ffc000",
+    readOnly: true,
+    spacing: "3px",
+
+  });
+
+
+  $('.featured-product__slider-inner').slick({
+
+  });
 
 
 
