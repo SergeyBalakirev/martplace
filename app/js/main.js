5588f902ce321af1fd1005f8ea053979a512a962
SergeyBalakirev <balakirev.sergey87@gmail.com>
2020-05-28 17:04:51 +0300

    added new-items section

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,15 @@ $(function () {
 
   });
 
+  $(".new-items__product-star").rateYo({
+    rating: 4.5,
+    starWidth: "11px",
+    ratedFill: "#ffc000",
+    readOnly: true,
+    spacing: "3px",
+
+  });
+
 
   $('.featured-product__slider-inner').slick({
     prevArrow: '<button class="slick-arrow slick-prev icon-lnr-chevron-left"></button>',
@@ -16,5 +25,15 @@ $(function () {
   });
 
 
+  $(".new-items__filter").on('click', function () {
+    $(".filter__box").slideToggle();
+  });
 
+
+
+
+
+
+
+  var mixer = mixitup('.new-items__product-box');
 });
